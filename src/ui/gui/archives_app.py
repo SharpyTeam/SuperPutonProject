c68fdda87a6c6d806df26645c242c0380227254f
Ilya <serverprom@yandex.ru>
2019-04-30 00:10:57 +0300

    Added some GUI events

--- src/ui/gui/archives_app.py
+++ src/ui/gui/archives_app.py
@@ -8,10 +8,18 @@ class ArchivesApp(QtWidgets.QMainWindow, archives_design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.menu.triggered.connect(self.exit_app)
+        self.pushButton.clicked.connect(self.change_label)
+
+    def change_label(self):
+        self.label.setText("Чего жмёшь, а?")
+
+    def exit_app(self):
+        self.close()
 
     @staticmethod
     def run():
         app = QtWidgets.QApplication(sys.argv)
         window = ArchivesApp()
         window.show()
-        app.exec()
+        sys.exit(app.exec())
